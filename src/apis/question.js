ab6a9ebe79a0efbf8b79efc1c5939eb887d17c75
lujingxiang <1067367579@qq.com>
2025-04-18 19:17:50 +0800

    完成问题列表开发

--- src/apis/question.js
+++ src/apis/question.js
@@ -1,5 +1,11 @@
-export function getQuestionListService() {
+import service from "@/utils/request";
 
+export function getQuestionListService(params) {
+    return service({
+        url: "/question/semiLogin/list",
+        method: "get",
+        params
+    })
 }
 
 export function getHotQuestionListService() {
